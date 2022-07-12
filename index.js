5f565393b3b0a4c6d7c8e5096e6af7698608cef0
trillionclues <trillionclues@gmail.com>
2022-07-12 03:42:22 +0100

    added modal overlay

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const checkbox = document.getElementById('checkbox')
+const modals = document.querySelectorAll('.modal')
+const modalCover = document.querySelector('.modal-overlay')
+const closeBtn = document.querySelector('.close-btn')
 
 checkbox.addEventListener('change', () => {
- // change website theme
- document.body.classList.toggle('dark')
+  // change website theme
+  document.body.classList.toggle('dark')
+})
+
+modals.forEach((elements) => {
+  // iterate throught the modal elements and apply the overlay to the target(selected) elements
+  elements.addEventListener('click', function () {
+    modalCover.classList.add('open-modal')
+  })
+})
+
+closeBtn.addEventListener('click', function () {
+  modalCover.classList.remove('open-modal')
 })
