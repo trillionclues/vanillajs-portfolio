a5354cbfe968128962fbbcd14a8ccd99f2c8e699
trillionclues <trillionclues@gmail.com>
2022-07-13 19:56:44 +0100

    fixed modal overlay

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 const checkbox = document.getElementById('checkbox')
 const modals = document.querySelectorAll('.modal')
 const modalCover = document.querySelector('.modal-overlay')
+const modalContain = document.querySelector('.modal-container')
 const closeBtn = document.querySelector('.close-btn')
 
+// DARK MODE SWITCH
 checkbox.addEventListener('change', () => {
   // change website theme
   document.body.classList.toggle('dark')
 })
 
-modals.forEach((elements) => {
-  // iterate throught the modal elements and apply the overlay to the target(selected) elements
-  elements.addEventListener('click', function () {
-    modalCover.classList.add('open-modal')
-  })
-})
+// MODAL DESPLAY
+const showModal = (openButton, modalContent) => {
+  const openBtn = document.getElementById(openButton)
+  const modalContainer = document.getElementById(modalContent)
 
-closeBtn.addEventListener('click', function () {
-  modalCover.classList.remove('open-modal')
-})
+  if (openBtn && modalContainer) {
+    openBtn.addEventListener('click', () => {
+      modalContainer.classList.add('show-modal')
+    })
+  }
+}
+
+showModal('open_modal', 'modal-container')
+
+// CLOSE MODAL
+const closeBtns = document.querySelectorAll('.close-modal')
+
+function closeModal() {
+  const modalContainer = document.getElementById('modal-container')
+  modalContainer.classList.remove('show-modal')
+}
+
+closeBtns.forEach((mod) => mod.addEventListener('click', closeModal))
